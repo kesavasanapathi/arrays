26fcec8092946b367aaf6ffdb859ac8e2b4c31af
kesavasanapathi <84114804+kesavasanapathi@users.noreply.github.com>
2021-05-28 11:20:37 +0400

    Add files via upload

---
+++ sumofarray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the n value";
+	cin>>n;
+	int a[n],l=0;
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"enter arry elements";
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		l=l+a[i];
+	}
+	cout<<l;
+}
+		
