031be91db8a94ee998a566b9189ad4a78eb29210
kesavasanapathi <84114804+kesavasanapathi@users.noreply.github.com>
2021-05-28 11:20:01 +0400

    Add files via upload

---
+++ sumofpositiveinarray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the n value";
+	cin>>n;
+	int a[n],l=0;
+	
+	for(int i=0;i<n;i++)
+	{
+		cout<<"enter arry elements";
+		cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]>0)
+		{
+		l=l+a[i];
+     	}
+	}
+	cout<<l;
+}
+		
